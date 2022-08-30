40b0498a620d3d267d69168d28d274ccdc8be828
Agrinsadon <111856786+Agrinsadon@users.noreply.github.com>
2022-08-30 15:23:42 +0300

    moduuli 4 tehtävät

---
+++ moduuli 4/tehtävä 4.5.py
@@ -0,0 +1,18 @@
+valid_username = "python"
+valid_password = "rules"
+
+tryCount= 0
+
+while tryCount < 5:
+    #tryCount = tryCount + 1
+    #tryCount maximi = tryCount < maxTries:
+
+
+    tryCount += 1
+    input_username = input("Käyttäjätunnus? ")
+    input_password = input("Salasana? ")
+    if valid_username == input_username and input_password == valid_password:
+        print("Tervetuloa!")
+        break
+    else:
+        print("Pääsy evätty!")
