b63955f572e66208af41d5f3cbc0e9d5970be4c8
Agrinsadon <111856786+Agrinsadon@users.noreply.github.com>
2022-08-31 15:16:13 +0300

    Moduuli 3 tehtävät

--- moduuli 3/tehtävä 3.1.py
+++ moduuli 3/tehtävä 3.1.py
@@ -1,6 +1,8 @@
 kuha = float(input("Kuinka pitkä on kuha (cm)?"))
-if kuha <= 37:
+if kuha < 37:
     print(f"Laske kuha takaisin järveen,kuhasta puuttuu {37 - kuha}(cm)")
+else:
+    print("Kuha on hyvän pituinen voit ottaa pois.")
 
 
 
